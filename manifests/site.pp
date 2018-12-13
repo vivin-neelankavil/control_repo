6b47b5bbe555eb6b1998dacf041c7f2d2c0f792d
Vivin Neelankavil Louis <vivin007@outlook.com>
2018-12-13 20:52:22 +0530

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,7 @@
 node default {
   file {'/root/README':
-    ensure => file,
-    content => 'This a readme after changing site.pp',
+    ensure  =>  file,
+    content =>  'This a readme after changing site.pp',
+    owner   =>  'root'
    }
 }
