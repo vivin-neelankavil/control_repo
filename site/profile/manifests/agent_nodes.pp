25b653e7ed81efc84110e4877a7eed553c3c67be
Vivin Neelankavil Louis <vivin007@outlook.com>
2018-12-15 12:23:09 +0530

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -2,4 +2,12 @@ class profile::agent_nodes {
   include dockeragent
   dockeragent::node {'web.puppet.vm':}
   dockeragent::node {'db.puppet.vm':}
+  hosts {'web.puppet.vm':
+    ensure  =>  present
+    ip      =>  "172.18.0.2"
+  }
+  hosts {'web.puppet.vm':
+    ensure  =>  present
+    ip      =>  "172.18.0.3"
+  }
 }
