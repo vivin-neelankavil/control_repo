7e59ccf0ef40d69127ba7b86a11c99e5293ea941
Vivin Neelankavil Louis <vivin007@outlook.com>
2018-12-15 12:11:54 +0530

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '/root/.ssh/id_rsa',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC6XmEVXSK27Pq7GJRVyminALvKGp3UAl2nooEpefJy6i32/Ib/rJKhpDeJQP0wFv3lHSWQHLNwRJ5WdxVmF1zA9GxusZEkfhoj0UXRjCdE6YI2H4metrAgDr6+a9flu4jm3uHSBOwalaInA0pnJ6owGm+NvzJvuOQ9lNOUSB3sIrmUO4J32+FzWVTRHZxh8fdMoaNyinDrVmh9LE0ZowCts8vdKa+cCGmR+hElFcuG3azzeea2CDqSlO0bq7TB95u9rP07D2jJv/bdYFlaqJ2kZ4PvMg+aCO2U220hZLhLUFrQe+XPSyR1rzLkbDIvDTS+B5HxE3FNSwx6yqJX/DRJ',
 	}  
 }
